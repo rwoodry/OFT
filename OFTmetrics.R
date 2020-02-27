01402d9542471037f5a01fa2eb7cfcb50c9768ec
rwoodry <rwoodry@gmail.com>
2020-02-27 10:56:46 -0800

    updated cmpile participant data with SDE and kmeans metrics. Error arises in concatenating files within the for loop.

--- OFTmetrics.R
+++ OFTmetrics.R
@@ -90,23 +90,82 @@ participant_list <- split(oft_data, as.factor(oft_data$Subject_ID))
 
 
 compile_participantmetrics <- function(participant_list){
+  part_master <- c()
   for (i in 1:length(participant_list)){
+    print(i)
     sub_data <- participant_list[[i]][grepl("TestScene", participant_list[[i]]$trial_level), ]
+    
+    subj_id <- participant_list[[i]]$Subject_ID[1]
     
     oall_means <- colMeans(as.data.frame(sub_data)[,4:23])[c(6:8, 10:12, 15:20)]
     oall_sds <- colSds(as.matrix(as.data.frame(sub_data)[,4:23]))[c(6:8, 10:12, 15:20)]
     
     bytarget_data <- split(sub_data, sub_data$target_obj)
-
+    
     chair_means <- colMeans(as.data.frame(bytarget_data$chFolding.A_LOD0)[,4:23])[c(6:8, 10:12, 15:20)]
     sball_means <- colMeans(as.data.frame(bytarget_data$'Soccer Ball')[,4:23])[c(6:8, 10:12, 15:20)]
     cone_means <- colMeans(as.data.frame(bytarget_data$cone_clean)[,4:23])[c(6:8, 10:12, 15:20)]
     bucket_means <- colMeans(as.data.frame(bytarget_data$Bucket_clean)[,4:23])[c(6:8, 10:12, 15:20)]
     
-    chair_sds <- colSds(as.data.frame(bytarget_data$chFolding.A_LOD0)[,4:23])[c(6:8, 10:12, 15:20)]
-    sball_sds <- colSds(as.data.frame(bytarget_data$'Soccer Ball')[,4:23])[c(6:8, 10:12, 15:20)]
-    cone_sds <- colSds(as.data.frame(bytarget_data$cone_clean)[,4:23])[c(6:8, 10:12, 15:20)]
-    bucket_sds <- colSds(as.data.frame(bytarget_data$Bucket_clean)[,4:23])[c(6:8, 10:12, 15:20)]
+    chair_sds <- colSds(as.matrix(bytarget_data$chFolding.A_LOD0[,4:23]))[c(6:8, 10:12, 15:20)]
+    sball_sds <- colSds(as.matrix(bytarget_data$'Soccer Ball'[,4:23]))[c(6:8, 10:12, 15:20)]
+    cone_sds <- colSds(as.matrix(bytarget_data$cone_clean[,4:23]))[c(6:8, 10:12, 15:20)]
+    bucket_sds <- colSds(as.matrix(bytarget_data$Bucket_clean[,4:23]))[c(6:8, 10:12, 15:20)]
+    
+    # Standard Deviation of the Ellipse metrics
+    calc_sde(id = 1, points = cbind(bytarget_data$chFolding.A_LOD0$end_x, bytarget_data$chFolding.A_LOD0$end_z))
+    sdeatt_chair <- sdeatt
+    calc_sde(id = 2, points = cbind(bytarget_data$'Soccer Ball'$end_x, bytarget_data$'Soccer Ball'$end_z))
+    sdeatt_sball <- sdeatt
+    calc_sde(id = 3, points = cbind(bytarget_data$Bucket_clean$end_x, bytarget_data$Bucket_clean$end_z))
+    sdeatt_bucket <- sdeatt
+    calc_sde(id = 4, points = cbind(bytarget_data$cone_clean$end_x, bytarget_data$cone_clean$end_z))
+    sdeatt_cone <- sdeatt
+    
+    # K- Means Cluster (clusters = 4)
+    fit <- kmeans(sub_data[,7:8], 4)
+    sub_data <- cbind(sub_data, fit$cluster)
+    
+    kmeans_ss_ratio <- fit$betweenss / fit$totss
+    kc_1_endx <- fit$centers[1, 1]
+    kc_1_endy <- fit$centers[1, 2]
+    kc_2_endx <- fit$centers[2, 1]
+    kc_2_endy <- fit$centers[2, 2]
+    kc_3_endx <- fit$centers[3, 1]
+    kc_3_endy <- fit$centers[3, 2]
+    kc_4_endx <- fit$centers[4, 1]
+    kc_4_endy <- fit$centers[4, 2]
+    
+    kc_1_size <- fit$size[1]
+    kc_2_size <- fit$size[2]
+    kc_3_size <- fit$size[3]
+    kc_4_size <- fit$size[4]
+    
+    kc_1_wss <- fit$withinss[1]
+    kc_2_wss <- fit$withinss[2]
+    kc_3_wss <- fit$withinss[3]
+    kc_4_wss <- fit$withinss[4]
+    
+    sbc<- split(sub_data, fit$cluster)
+    
+    kc_1_hg <- max(table(sbc$'1'$target_obj)) / nrow(sbc$'1')
+    kc_2_hg <- max(table(sbc$'2'$target_obj)) / nrow(sbc$'2')
+    kc_3_hg <- max(table(sbc$'3'$target_obj)) / nrow(sbc$'3')
+    kc_4_hg <- max(table(sbc$'4'$target_obj)) / nrow(sbc$'4')
+    
+    # TODO: FIX THIS
+    part_data <- c(subj_id, oall_means, oall_sds, 
+                   chair_means, chair_sds, sball_means, sball_sds, 
+                   bucket_means, bucket_sds, cone_means, cone_sds,
+                   sdeatt_chair, sdeatt_sball, sdeatt_bucket, sdeatt_cone,
+                   kmeans_ss_ratio, 
+                   kc_1_endx, kc_1_endy, kc_1_size, kc_1_wss, kc_1_hg,
+                   kc_2_endx, kc_2_endy, kc_2_size, kc_2_wss, kc_2_hg,
+                   kc_3_endx, kc_3_endy, kc_3_size, kc_3_wss, kc_3_hg,
+                   kc_4_endx, kc_4_endy, kc_4_size, kc_4_wss, kc_4_hg)
+    print(part_data)
+    part_master <- rbind(part_master, part_data)
   }
+  
+  return(part_data)
 }
-
