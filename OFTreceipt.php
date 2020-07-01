a86be85a7cf39ef96485194f7c4f8989db62fbf0
rwoodry <41202858+rwoodry@users.noreply.github.com>
2020-07-01 16:07:40 -0700

    Add files via upload
    Updated PHP to collect both position and trial data.

--- OFTreceipt.php
+++ OFTreceipt.php
@@ -1,4 +1,17 @@
 <?php
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	    header('Access-Control-Allow-Origin: *');
+	    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+	    //header('Access-Control-Allow-Methods: POST, OPTIONS');
+	    header('Access-Control-Allow-Headers: token, Content-Type');
+	    header('Access-Control-Max-Age: 1728000');
+	    header('Content-Length: 0');
+	    header('Content-Type: text/plain');
+	    die();
+	}
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+
 	$text1 = $_POST["input"];
 	$filename = $_POST["filename"];
 
@@ -7,6 +20,19 @@
 		$file = fopen($filename, "a"); 
 		fwrite($file, $text1);
 		fclose($file);
+	} else
+	{
+		echo("Messsage delivery failed...");
+	}
+
+	$text2 = $_POST["input2"];
+	$filename2 = $_POST["filename2"];
+
+	if ($text2 != "")
+	{
+		$file2 = fopen($filename2, "a"); 
+		fwrite($file2, $text2);
+		fclose($file2);
 	} else
 	{
 		echo("Messsage delivery failed...");
