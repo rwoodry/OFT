d1547a3f2036a5f6e3b3340f35a4a8ec178448c0
rwoodry <41202858+rwoodry@users.noreply.github.com>
2020-07-31 20:41:47 -0700

    Add files via upload
    Fixed automation bugs.

--- OFTtoDrive.py
+++ OFTtoDrive.py
@@ -12,10 +12,11 @@ import glob
 
 # Define function to upload OFT files to lab's UCI google drive
 def upload_OFTfiles():
+	print("FILES UPLOADING TO DRIVE")
 	# Initialize authorization
 	g_login = GoogleAuth()
 	# Try to load saved credentials
-	g_login.LoadCredentialsFile("mycreds.txt")
+	g_login.LoadCredentialsFile("/home/chrastillab/Desktop/mycreds.txt")
 	if g_login.credentials is None:
 	    # Authenticate if they're not there
 	    g_login.LocalWebserverAuth()
@@ -27,7 +28,7 @@ def upload_OFTfiles():
 	    g_login.Authorize()
 
 	# Save the current credentials to a file
-	g_login.SaveCredentialsFile("mycreds.txt")
+	g_login.SaveCredentialsFile("/home/chrastillab/Desktop/mycreds.txt")
 	drive = GoogleDrive(g_login)
 
 	# Within OFT file folder, obtain all OFT_filenames
@@ -41,12 +42,30 @@ def upload_OFTfiles():
 
 	# Loop through OFT files
 	for i in OFT_filenames:
+		# If file already exists, overwrite it with the most recent version
+		file_list = drive.ListFile({'q':"'1AAAYH0bWciY2aS4Qtdxr6ZemcPTOziKz' in parents and trashed=False"}).GetList()
+		try:
+			for file1 in file_list:
+				if file1['title'] == os.path.join(path, i):
+					print("Overwriting file: " + os.path.join(path, i))
+					file1.Delete()                
+		except:
+			pass
+		# Create file to send to the drive, then upload it
 		f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": f_ID}]})
 		f.SetContentFile(os.path.join(path, i))
 		f.Upload()
+		print("Uploading file: " + os.path.join(path, i))
 		f = None
+
+	print("FILES UPLOADED TO DRIVE")
+
+# Initiaite first upload when script is called. Keep terminal window open to have the scheduler run.
+upload_OFTfiles()
 
 # Initiate scheduled upload of OFT files every hour
 scheduler = BlockingScheduler()
 scheduler.add_job(upload_OFTfiles, 'interval', hours = 1)
+scheduler.start()
+
 
