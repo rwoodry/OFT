9f7aa3f8cc479291158f82e65067272d297d3e1b
rwoodry <41202858+rwoodry@users.noreply.github.com>
2021-02-02 18:54:20 -0800

    Add files via upload

--- SOFTtoDrive.py
+++ SOFTtoDrive.py
@@ -43,11 +43,13 @@ def upload_SOFTfiles():
 	OFT_filenames = glob.glob('*.{}'.format(extension))
 	# Hellman folder ID for OFT
 	OFT_folder_ID = "1gThYbi0REw5SFgdv2DB0xR3d1vC89VBF"
-	OFT_shared_ID = "136zAoJbvmplv0aDczGppFAFXYUOCFi9D"
+	OFT_shared_ID = "1tCYCn7zIduvGeR6pRYL5YaS84EpGegTu"
 	
 	# Upload OFT files to regular and shared drives	
-	drive_upload_to_folder(OFT_filenames, OFT_folder_ID, drive)
+	print("\n---OFT [SHARED] Backup---")
 	drive_upload_to_folder(OFT_filenames, OFT_shared_ID, drive)
+	print("\n---OFT Backup---")
+	drive_upload_to_folder(OFT_filenames, OFT_folder_ID, drive)
 	print("OFT DATA FILES UPLOADED TO DRIVE")
 
 	# Within SOT file folder, obtain all SOT_filenames
@@ -57,10 +59,12 @@ def upload_SOFTfiles():
 	SOT_filenames = glob.glob('*.{}'.format(extension))
 	# Hellman folder ID for SOT
 	SOT_folder_ID = "10Kvhc_fjTM2S4TOfGSx0MMWs0Z8ec7CL"
-	SOT_shared_ID = "1bqmsA12vkAd-xdkVtv18NdgUcj5bmoWf"
+	SOT_shared_ID = "1sWI_4BfIajYnIO2YBLRWNNyA9FidqkhY"
 	
 	# Upload SOT files to regular and shared drives
+	print("\n---SOT Backup---")
 	drive_upload_to_folder(SOT_filenames, SOT_folder_ID, drive)
+	print("\n---SOT [SHARED] Backup---")
 	drive_upload_to_folder(SOT_filenames, SOT_shared_ID, drive)
 	print("SOT DATA FILES UPLOADED TO DRIVE")
 
@@ -71,25 +75,32 @@ def quality_check_SOFT():
 
 def drive_upload_to_folder(filenames, folder_id, drive):
 	query_string = "'%s' in parents and trashed=False" % folder_id
-	for i in filenames:
+	for index, i in enumerate(filenames):
 		# If file already exists, overwrite it with the most recent version
-		file_list = drive.ListFile({'q': query_string}).GetList()
+		file_list = drive.ListFile({'q': query_string, 'supportsAllDrives': True, 'includeItemsFromAllDrives': True}).GetList()
 
 		try:
 			for file1 in file_list:
 				if file1['title'] == i:
-					print("Overwriting file: " + i)
 					file1.Delete()
 		except:
 			pass
 		# Create file to send to the drive, then upload it
-		f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}]})
+		f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}], 'supportsAllDrives': True, 'includeItemsFromAllDrives': True})
 		f.SetContentFile(i)
 
-		f.Upload()
+		f.Upload({'supportsAllDrives': True})
 
-		print("Uploading file: " + i)
+		progressBar(index, len(filenames))
 		f = None
+
+
+def progressBar(current, total, barLength = 20):
+    percent = float(current) * 100 / total
+    arrow   = '-' * int(percent/100 * barLength - 1) + '>'
+    spaces  = ' ' * (barLength - len(arrow))
+
+    print('Progress: [%s%s] %d %% %03d/%03d' % (arrow, spaces, percent, current, total), end='\r')
 
 # Initiate first upload when script is called. Keep terminal window open to have the scheduler run.
 upload_SOFTfiles()
